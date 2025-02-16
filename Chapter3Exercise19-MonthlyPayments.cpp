6ccb93fb62d8f6b384107dfc938004884ea86d7d
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-16 18:40:46 -0500

    Original Code

--- Chapter3Exercise19-MonthlyPayments.cpp
+++ Chapter3Exercise19-MonthlyPayments.cpp
@@ -5,26 +5,56 @@ Date: February 2025
 Requirements:
 Write a program that asks for these values then displays a report similar to:
 Loan Amount: $10,000
-Monthly Interest 
-
-
-
+Monthly Interest Rate: 1%
+Number of Payments: 36
+Monthly Payments: $332.14
+Amount Paid Back: $11957.15
+Interest Paid: $1957.15
 */
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double mRate;
+    int nPayments = 36;
+    double Loan = 10000.00;
+    
+    cout << "What is the monthly interest rate for your interest?: ";
+    cin >> mRate;
+
+    mRate = mRate / 100;
+
+    cout << "What is the number of payments you have made?: ";
+    cin >> nPayments;
+    cout << "What is the amount of the loan?: $";
+    cin >> Loan;
+    
+    double topinner = (1 + mRate);
+    double topdiv = mRate * (pow(topinner, nPayments));
+    double bottominner = (1 + mRate);
+    double bottomdiv = ((pow(bottominner, nPayments)) - 1);
+    double mPayment = (topdiv / bottomdiv) * Loan;
+    double amountPayb = mPayment * nPayments;
+    double intPaid = amountPayb - Loan;
+
+    cout << "Loan Amount: "<< setw(19)<< "$"<<Loan << endl;
+    cout << "Monthly Interest Rate: " << setw(12) << mRate << endl;
+    cout << "Number of Payments: " << setw(13) << nPayments << endl;
+    cout << "Monthly Payment: " << setw(15) << "$"<< mPayment << endl;
+    cout << "Amount Paid Back: " << setw(14) << "$"<< amountPayb << endl;
+    cout << "Interest Paid: " << setw(17) << "$"<<intPaid << endl;
+
+
+
+
+
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
